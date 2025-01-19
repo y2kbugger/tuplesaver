43ec3c48926cca36445c473a422c676685c7fd3e
y2k <git@y2kbugger.com>
2025-01-19 14:11:44 -0500

    Clean up and organize query.py

--- micro_namedtuple_sqlite_persister/query.py
+++ micro_namedtuple_sqlite_persister/query.py
@@ -1,8 +1,8 @@
 from _collections import _tuplegetter  # type: ignore
 from collections.abc import Sequence
-from typing import Any, NamedTuple
+from typing import Any, cast
 
-from .persister import Row, is_row_model
+from .model import Row, is_row_model
 
 
 def get_field_idx(field: Any) -> int:
@@ -26,77 +26,35 @@ def get_field_idx(field: Any) -> int:
     return field.__reduce__()[1][0]
 
 
-def get_column_name(Model: type[NamedTuple], idx: int) -> str:
+def get_column_name(Model: type[Row], idx: int) -> str:
     return f"{Model.__name__}.{Model._fields[idx]}"
 
 
-def get_table_name(Model: type[NamedTuple]) -> str:
+def get_table_name(Model: type[Row]) -> str:
     return Model.__name__
 
 
 # fmt: off
-class SELECT(tuple): pass  # SELECT statement  # noqa: E701
-class CSV(tuple): pass  # command separated values  # noqa: E701
-class WHERE(tuple): pass  # WHERE clause  # noqa: E701
-class LIMIT(tuple): pass  # LIMIT clause  # noqa: E701
+class SELECT(tuple): ...
+class CSV(tuple): ...  # comma separated values
+class WHERE(tuple): ...
+class LIMIT(tuple): ...
+class LOGIC(tuple): ...  # logical operator
+class STRLIT(str): ...  # string literal
 class CMP(tuple): # comparison
     def __new__(cls, args: tuple) -> tuple:
-        l, o, r = args # noqa: E741
-        if isinstance(l, str):
-            l = STRLIT(l) # noqa: E741
-        if isinstance(r, str):
-            r = STRLIT(r)
-        return super().__new__(cls, (l, o, r))
-class LOGIC(tuple): pass  # logical operator # noqa: E701
-class STRLIT(str): pass  # string literal  # noqa: E701
+        left, op, right = args
+        if isinstance(left, str):
+            left = STRLIT(left)
+        if isinstance(right, str):
+            right = STRLIT(right)
+        return super().__new__(cls, (left, op, right))
 # fmt: on
 
 type Field = _tuplegetter
 type Scalar = Field | str | int | float
 type Frag = Field | Scalar | CSV
 type Fragment = Sequence[Frag | Fragment] | Frag
-
-
-def _select(Model: type[Row], *, where: Fragment | None = None, limit: int | None = None) -> Fragment:
-    """Create a SELECT statement"""
-    cols = CSV(Model._fields)
-    select = ('SELECT', cols, 'FROM', Model)
-    if where is not None:
-        select = (*select, WHERE(('WHERE', where)))
-    if limit is not None:
-        select = (*select, LIMIT(("LIMIT", limit)))
-    return SELECT(select)
-
-
-def render(M: type[Row], fg: Fragment) -> str:
-    match fg:
-        case SELECT((select, cols, frm, model, *rest)):  # Deconstruct SELECT statement
-            return f"{select} {render(M, cols)}\n{frm} {render(M, model)}" + (f"\n{'\n'.join(render(M, f) for f in rest)}" if rest else "")
-        case CSV(fields):  # Deconstruct CSV into fields
-            return ', '.join(fields)
-        case CMP((left, op, right)):  # Deconstruct CMP into left, op, and right
-            return f"({render(M, left)} {op} {render(M, right)})"
-        case LOGIC((left, operator, right)):  # Deconstruct LOGIC into left and right
-            return f"({render(M, left)} {operator} {render(M, right)})"
-        case WHERE((keyword, condition)):  # Deconstruct WHERE into keyword and condition
-            return f"{keyword} {render(M, condition)}"
-        case LIMIT((keyword, value)):  # Deconstruct LIMIT into keyword and value
-            return f"{keyword} {value}"
-        case _tuplegetter():
-            assert M is not None
-            return get_column_name(M, get_field_idx(fg))
-        case _ if is_row_model(fg):
-            return M.__name__
-        case tuple():  # Match any tuple and deconstruct into elements
-            return ' '.join(render(M, f) for f in fg)
-        case int() | float():
-            return str(fg)
-        case STRLIT():
-            return f"'{fg}'"
-        case str():
-            return fg
-        case _:
-            raise TypeError(f"Unexpected type: {type(fg)}")
 
 
 def select[R: Row](Model: type[R], *, where: Fragment | None = None, limit: int | None = None) -> tuple[type[R], str]:
@@ -141,3 +99,45 @@ def or_(left: Fragment, right: Fragment) -> Fragment:
 
 def and_(left: Fragment, right: Fragment) -> Fragment:
     return LOGIC((left, 'AND', right))
+
+
+def _select(Model: type[Row], *, where: Fragment | None = None, limit: int | None = None) -> Fragment:
+    """Create a SELECT statement"""
+    cols = CSV(Model._fields)
+    select = ('SELECT', cols, 'FROM', Model)
+    if where is not None:
+        select = (*select, WHERE(('WHERE', where)))
+    if limit is not None:
+        select = (*select, LIMIT(("LIMIT", limit)))
+    return SELECT(select)
+
+
+def render(M: type[Row], fg: Fragment) -> str:
+    match fg:
+        case SELECT((select, cols, frm, model, *rest)):  # Deconstruct SELECT statement
+            return f"{select} {render(M, cols)}\n{frm} {render(M, model)}" + (f"\n{'\n'.join(render(M, f) for f in rest)}" if rest else "")
+        case CSV(fields):  # Deconstruct CSV into fields
+            return ', '.join(fields)
+        case CMP((left, op, right)):  # Deconstruct CMP into left, op, and right
+            return f"({render(M, left)} {op} {render(M, right)})"
+        case LOGIC((left, operator, right)):  # Deconstruct LOGIC into left and right
+            return f"({render(M, left)} {operator} {render(M, right)})"
+        case WHERE((keyword, condition)):  # Deconstruct WHERE into keyword and condition
+            return f"{keyword} {render(M, condition)}"
+        case LIMIT((keyword, value)):  # Deconstruct LIMIT into keyword and value
+            return f"{keyword} {value}"
+        case _tuplegetter():
+            assert M is not None
+            return get_column_name(M, get_field_idx(fg))
+        case _ if is_row_model(fg):
+            return get_table_name(cast(type[Row], fg))
+        case tuple():  # Match any tuple and deconstruct into elements
+            return ' '.join(render(M, f) for f in fg)
+        case int() | float():
+            return str(fg)
+        case STRLIT():
+            return f"'{fg}'"
+        case str():
+            return fg
+        case _:
+            raise TypeError(f"Unexpected type: {type(fg)}")
