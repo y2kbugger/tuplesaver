8738e7025bff9832a7433b75cb7ae4963d6151f7
y2k <git@y2kbugger.com>
2025-03-07 01:26:34 -0500

    Add, test, and examplify initial commit of query2

---
+++ micro_namedtuple_sqlite_persister/query2.py
@@ -0,0 +1,99 @@
+from __future__ import annotations
+
+import ast
+import inspect
+from textwrap import dedent
+from typing import Callable
+
+from micro_namedtuple_sqlite_persister.model import Row, get_meta
+
+
+def select[R: type[Row]](Model: R) -> tuple[R, str]:
+    return Model, get_meta(Model).select
+
+
+def select_query[R: type[Row]](Model: R) -> Callable[[Callable], Callable[[], tuple[R, str]]]:
+    def decorator(query_def_func: Callable) -> Callable[[], tuple[R, str]]:
+        return lambda: (Model, render_query_def_func(Model, query_def_func))
+
+    return decorator
+
+
+def render_query_def_func(Model: type[Row], func: Callable) -> str:
+    source = inspect.getsource(func)
+    source = dedent(source)
+    tree = ast.parse(source)
+    fbody = tree.body[0].body  # type: ignore
+    assert len(fbody) == 1, "The query def must only contain a single expression"
+    expr = fbody[0]
+
+    js: ast.JoinedStr = expr.value
+    query_parts: list[str] = []
+    joins: dict[str, str] = {}
+
+    basemeta = get_meta(Model)
+    for v in js.values:
+        match v:
+            case ast.Constant():
+                assert isinstance(v.value, str), "This is supposed to be a string part of an f-string"
+                query_parts.append(v.value)
+            case ast.FormattedValue():
+                # We could use formats and conversions to do special things later if we want
+                assert v.format_spec is None, "Do not include format specifiers in the field names. e.g. {name:0.2f}"
+                assert v.conversion == -1, "Do not include conversion  in the field names. e.g. {!r}"
+
+                stack = []
+                for n in ast.walk(v.value):
+                    if isinstance(n, (ast.Attribute, ast.Name)):
+                        stack.append(n)
+                stack.reverse()
+                assert isinstance(stack[0], ast.Name), "The first part of the field name must be a class name"
+                assert all(isinstance(n, ast.Attribute) for n in stack[1:]), "The rest of the field name must be attributes"
+
+                ModelName = stack[0].id
+                assert Model.__name__ == ModelName, f"Specify all columns as field paths from {Model.__name__}, e.g. {Model.__name__}.foo.bar.name"
+                found_intermediatte_field = None
+                if len(stack) > 2:
+                    join_alias_parts = []
+                    meta = basemeta
+                    last_jalias = basemeta.table_name
+                    for attrlevel in stack[1:-1]:
+                        join_alias_parts.append(attrlevel.attr)
+                        # TODO: save this looping by add a field by name dict to meta
+                        for field in meta.fields:
+                            if field.name == attrlevel.attr:
+                                found_intermediatte_field = field
+                                meta = get_meta(field.type)
+                                jalias = '_'.join(join_alias_parts)
+                                joins[jalias] = f"JOIN {meta.table_name} {jalias} ON {last_jalias}.{field.name} = {jalias}.id"
+                                last_jalias = '_'.join(join_alias_parts)
+
+                                break
+                        assert found_intermediatte_field is not None, f"Field {attrlevel.attr} not found in {ModelName}"
+                    jalias = '_'.join(join_alias_parts)
+                    table_or_alias = jalias
+                    finalmeta = meta
+                else:
+                    table_or_alias = basemeta.table_name
+                    finalmeta = basemeta
+                final_field = None
+                # TODO: save this looping by add a field by name dict to meta
+                for field in finalmeta.fields:
+                    if field.name == stack[-1].attr:
+                        final_field = field
+                        break
+                assert final_field is not None, f"Field {stack[-1].attr} not found in {ModelName}"
+                query_parts.append(table_or_alias + "." + final_field.name)
+            case _:
+                print("Unknown type", type(v), v)
+                raise ValueError("Unknown type in query f-string")
+
+    select = get_meta(Model).select
+    query_predicate = dedent("".join(query_parts)).strip()
+    query_parts = [query_predicate]
+
+    join_clauses = joins.values()
+    if len(join_clauses) > 0:
+        query_parts.insert(0, "\n".join(join_clauses) + "\n")
+    query_parts.insert(0, select + "\n")
+    return "".join(query_parts)
---
+++ micro_namedtuple_sqlite_persister/query2_test.py
@@ -0,0 +1,71 @@
+from textwrap import dedent
+from typing import NamedTuple
+
+from .query2 import select, select_query
+
+
+class League(NamedTuple):
+    id: int | None
+    leaguename: str
+
+
+class Team(NamedTuple):
+    id: int | None
+    teamname: str
+    league: League
+
+
+class Athlete(NamedTuple):
+    id: int | None
+    name: str
+    team: Team
+
+
+def dd(sql: str) -> str:
+    return dedent(sql).strip()
+
+
+def test_select_on_table() -> None:
+    M, q = select(Athlete)
+    assert q == dd("""
+        SELECT Athlete.id, Athlete.name, Athlete.team FROM Athlete
+        """)
+
+
+def test_select_on_table_with_where() -> None:
+    @select_query(Athlete)
+    def big_leagues():
+        return f"WHERE {Athlete.name} = 'Joe'"
+
+    M, q = big_leagues()
+    assert q == dd("""
+        SELECT Athlete.id, Athlete.name, Athlete.team FROM Athlete
+        WHERE Athlete.name = 'Joe'
+        """)
+
+
+def test_select_on_table_with_join_caused_by_predicate() -> None:
+    @select_query(Athlete)
+    def athletes_on_red_team():
+        return f"WHERE {Athlete.team.teamname} = 'Red Snickers'"
+
+    M, q = athletes_on_red_team()
+    assert q == dd("""
+        SELECT Athlete.id, Athlete.name, Athlete.team FROM Athlete
+        JOIN Team team ON Athlete.team = team.id
+        WHERE team.teamname = 'Red Snickers'
+        """)
+
+
+def test_select_on_table_with_multiple_implicit_joins() -> None:
+    @select_query(Athlete)
+    def athletes_in_big_league():
+        return f"WHERE {Athlete.team.league.leaguename} = 'Big'"
+
+    M, q = athletes_in_big_league()
+    assert q == dd("""
+        SELECT Athlete.id, Athlete.name, Athlete.team FROM Athlete
+        JOIN Team team ON Athlete.team = team.id
+        JOIN League team_league ON team.league = team_league.id
+        WHERE team_league.leaguename = 'Big'
+        """)
